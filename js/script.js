838f43b0d9d6e96be80f95a18f4f7dfed4ab3816
Iann-rst <iannrst11@gmail.com>
2021-08-17 12:19:17 -0300

    arquivo js para manipular o carrossel

--- js/script.js
+++ js/script.js
@@ -1,7 +1,14 @@
+/**
+ * loop - propriedade para as img ficar em loop, ao passar para o lado.
+ * margin - margem entre as imagens do carrossel.
+ * nav - barra de navegação em baixo das imagens do carrossel.
+ * responsive - quantidade de imagens de acordo com o tamanho da tela.
+ */
+
 $('.owl-carousel').owlCarousel({
   loop:true,
   margin:10,
-  nav:true,
+  nav:false,
   responsive:{
       0:{
           items:1
