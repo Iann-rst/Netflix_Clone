ea1285406ca21e897ae25855e4d463b94f9db380
Iann-rst <iannrst11@gmail.com>
2022-08-26 14:34:51 -0300

    README

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Netflix_Clone
+
 Recriando a interface do site Netflix utilizando HTML5, CSS3 E JavaScript.
+
+
+## :camera: Screenshot
+
+<div align="center">
+  <img src="./github/home.png" alt="">
+</div>
--- js/script.js
+++ js/script.js
@@ -6,18 +6,18 @@
  */
 
 $('.owl-carousel').owlCarousel({
-  loop:true,
-  margin:10,
-  nav:false,
-  responsive:{
-      0:{
-          items:1
-      },
-      600:{
-          items:3
-      },
-      1000:{
-          items:5
-      }
-  }
+    loop: true,
+    margin: 10,
+    nav: false,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 3
+        },
+        1000: {
+            items: 5
+        }
+    }
 })
